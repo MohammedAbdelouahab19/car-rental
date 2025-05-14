dd03f2417487d50d45d47e3935833f4a6e8a5870
mohammed abdelouahab <mohamedabdelouahab0@gmail.com>
2025-05-14 23:26:48 +0100

    regenerate migration

---
+++ migrations/Version20250514222506.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250514222506 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE car (id SERIAL NOT NULL, title VARCHAR(255) NOT NULL, brand VARCHAR(50) NOT NULL, price DOUBLE PRECISION NOT NULL, description TEXT DEFAULT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE refresh_token (id SERIAL NOT NULL, refresh_token VARCHAR(128) NOT NULL, username VARCHAR(255) NOT NULL, valid TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE UNIQUE INDEX UNIQ_C74F2195C74F2195 ON refresh_token (refresh_token)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE reservation (id SERIAL NOT NULL, car_id INT NOT NULL, tenant_id INT NOT NULL, start_date DATE NOT NULL, end_date DATE NOT NULL, total_price DOUBLE PRECISION NOT NULL, reference VARCHAR(255) NOT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_42C84955C3C6F69F ON reservation (car_id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_42C849559033212A ON reservation (tenant_id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE "user" (id SERIAL NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) DEFAULT NULL, password_updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, roles JSON NOT NULL, phone_number VARCHAR(50) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE UNIQUE INDEX user_unique ON "user" (username)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reservation ADD CONSTRAINT FK_42C84955C3C6F69F FOREIGN KEY (car_id) REFERENCES car (id) NOT DEFERRABLE INITIALLY IMMEDIATE
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reservation ADD CONSTRAINT FK_42C849559033212A FOREIGN KEY (tenant_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE SCHEMA public
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reservation DROP CONSTRAINT FK_42C84955C3C6F69F
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE reservation DROP CONSTRAINT FK_42C849559033212A
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE car
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE refresh_token
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE reservation
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE "user"
+        SQL);
+    }
+}
